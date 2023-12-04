a96fe17678902f1265a93f3ccd4edd74614375eb
yaretdiazjaimes <yaretjaimes23@gmail.com>
2023-12-04 16:40:16 -0600

    Funcionamiento correcto de LAZY

--- src/main/java/com/getechnologies/facturas/FacturasApplication.java
+++ src/main/java/com/getechnologies/facturas/FacturasApplication.java
@@ -1,10 +1,16 @@
 package com.getechnologies.facturas;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FacturasApplication {
+	@Bean
+	protected Hibernate5JakartaModule module() {
+		return new Hibernate5JakartaModule();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FacturasApplication.class, args);
